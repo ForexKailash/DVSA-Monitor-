1a5db39fe27a20fdb92473b552054ea430f7d459
ForexKailash <k8683934579@gmail.com>
2026-05-18 08:58:42 +0100

    Update monitor.py
    Bh

--- monitor.py
+++ monitor.py
@@ -1,18 +1,28 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+    return "DVSA Monitor Running"
 
 @app.route("/slots")
 def slots():
 
-    sample_data = {
+    return jsonify({
         "centres": [
             {"name": "Loughton"},
             {"name": "Goodmayes"}
         ]
-    }
-
-    return jsonify(sample_data)
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
