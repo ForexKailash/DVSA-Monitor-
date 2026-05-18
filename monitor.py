1f6e733e0993ef82d6628f0ed17fa0cdb793efe9
ForexKailash <k8683934579@gmail.com>
2026-05-18 09:04:33 +0100

    Update monitor.py
    Hhn

--- monitor.py
+++ monitor.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
     return "DVSA Monitor Running"
 
 @app.route("/slots")
@@ -22,7 +21,4 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
 
-    app.run(
-        host="0.0.0.0",
-        port=port
-    )
+    app.run(host="0.0.0.0", port=port)
